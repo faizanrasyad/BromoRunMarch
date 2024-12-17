7824ed4907a0b2c5ca6101ff78595df3c741e390
Faizan Rasyad <110027376+faizanrasyad@users.noreply.github.com>
2024-12-17 07:48:28 +0700

    Update PenerbanganForm.cs

--- PenerbanganForm.cs
+++ PenerbanganForm.cs
@@ -1,197 +1,232 @@
-﻿using BromoAirlineDesktop.CRUD;
-using BromoAirlineDesktop.Data;
+using BromoRunMarch.Data;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace BromoAirlineDesktop
+namespace BromoRunMarch
 {
-    public partial class PenerbanganForm : Form
+    public partial class ListPenerbanganForm : Form
     {
-        public PenerbanganForm()
+        public ListPenerbanganForm()
         {
             InitializeComponent();
         }
 
-        private void btnBack_Click(object sender, EventArgs e)
+        private void ListPenerbanganForm_Load(object sender, EventArgs e)
         {
-            MainFormCustomer cust = new MainFormCustomer();
-            this.Close();
-            cust.Show();
+            // TODO: This line of code loads data into the 'bromoRunMarchDataSet.JadwalPenerbangan' table. You can move, or remove it, as needed.
+            setupView();
+
         }
 
-        void setupAwal()
+        void setupView()
         {
-            lbBandaraAsal.Text = CustomerCRUD.bandaraAsal.nama + " ("+CustomerCRUD.bandaraAsal.kodeIATA + ")";
-            lbBandaraTujuan.Text = CustomerCRUD.bandaraTujuan.nama + " ("+CustomerCRUD.bandaraTujuan.kodeIATA + ")";
-            lbPenumpang.Text = CustomerCRUD.jumlahPenumpang + " Penumpang";
-            var hari = CustomerCRUD.tanggalLiburan.DayOfWeek;
-            var tanggal = CustomerCRUD.tanggalLiburan.Day;
-            var bulan = CustomerCRUD.tanggalLiburan.ToString("MMM");
-            var tahun = CustomerCRUD.tanggalLiburan.Year;
-            lbTanggal.Text = hari + ", " + tanggal + " " + bulan + " " + tahun;
-
             fillDGV();
+            lbBandaraAsal.Text = repoListPenerbangan.bandaraAsal.nama + " (" + repoListPenerbangan.bandaraAsal.kodeIATA + ")";
+            lbBandaraTujuan.Text = repoListPenerbangan.bandaraTujuan.nama + " (" + repoListPenerbangan.bandaraTujuan.kodeIATA + ")";
+            var tanggal = repoListPenerbangan.tanggal;
+            lbTanggal.Text = tanggal.DayOfWeek.ToString().Substring(0, 3) + ", " + tanggal.Day + " " + tanggal.ToString("MMMM") + " " + tanggal.Year;
+            lbJumlahPenumpang.Text = repoListPenerbangan.jumlahPenumpang.ToString() + " Penumpang";
         }
 
-        private void PenerbanganForm_Load(object sender, EventArgs e)
+        #region filtering
+        private void cbUrutkan_SelectionChangeCommitted(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'bromoAirlineDataSet.JadwalPenerbangan' table. You can move, or remove it, as needed.
             
-            setupAwal();
         }
 
-        void fillDGV()
+        private void btnFilter_Click(object sender, EventArgs e)
         {
-            this.jadwalPenerbanganTableAdapter.Fill(this.bromoAirlineDataSet.JadwalPenerbangan);
-            CurrencyManager cm = (CurrencyManager)BindingContext[dataGridView1.DataSource];
-            foreach(DataGridViewRow row in dataGridView1.Rows)
-            {
-                var bandaraAsal = row.Cells["bandaraKeberangkatanID"].Value;
-                var bandaraTujuan = row.Cells["bandaraTujuanID"].Value;
-                var tanggal = row.Cells["tanggalWaktuKeberangkatan"].Value;
-                Debug.Print("Bandara Asal Model : " + bandaraAsal);
-                Debug.Print("Bandara Asal CustomerCRUD : " + CustomerCRUD.bandaraAsal.id);
-                cm.SuspendBinding();
-                if (
-                    Convert.ToInt32(bandaraAsal) != CustomerCRUD.bandaraAsal.id &&
-                    Convert.ToInt32(bandaraTujuan) != CustomerCRUD.bandaraTujuan.id &&
-                    Convert.ToDateTime(tanggal) != CustomerCRUD.tanggalLiburan)
-                {
-                    row.Visible = false;
-                }
-                else
-                {
-                    row.Visible = true;
-                }
-
-                var modelBandaraAsal = Repo.entity.Bandaras.Find(row.Cells["bandaraKeberangkatanID"].Value);
-                var modelBandaraTujuan = Repo.entity.Bandaras.Find(row.Cells["bandaraTujuanID"].Value);
-                var modelMaskapai = Repo.entity.Maskapais.Find(row.Cells["maskapaiID"].Value);
-                var tanggalPenerbangan = Convert.ToDateTime( row.Cells["tanggalWaktuKeberangkatan"].Value );
-                var durasiPenerbangan = row.Cells["durasiPenerbangan"].Value;
-                var waktuPenerbangan = tanggalPenerbangan.ToString("HH:mm");
-
-                row.Cells["bandaraAsal"].Value = modelBandaraAsal.nama;
-                row.Cells["bandaraTujuan"].Value = modelBandaraTujuan.nama;
-                row.Cells["maskapai"].Value = modelMaskapai.nama;
-                row.Cells["tanggalPenerbangan"].Value = tanggalPenerbangan.ToString("dd-MM-yyyy");
-                row.Cells["waktuPenerbangan"].Value = waktuPenerbangan + " - " + durasiPenerbangan + " menit";
-                cm.ResumeBinding();
-            }
+            setupFilter();
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void setupFilter()
         {
-            if (dataGridView1.Columns[e.ColumnIndex].Name == "beliTiket")
-            {
-                var maskapaiID = dataGridView1.Rows[e.RowIndex].Cells["maskapaiID"].Value;
-                var idJadwal = dataGridView1.Rows[e.RowIndex].Cells["id"].Value;
-                Debug.Print("Maskapai ID : " + maskapaiID);
-                Debug.Print("Tipe Maskapai ID : " + maskapaiID.GetType());
-                Debug.Print("ID Jadwal : " + idJadwal);
-                Debug.Print("Tipe ID Jadwal : " + idJadwal.GetType());
-                CustomerCRUD.maskapai = Repo.entity.Maskapais.Find(maskapaiID);
-                CustomerCRUD.jadwalPenerbangan = Repo.entity.JadwalPenerbangans.Find(idJadwal);
+            fillDGV();
 
-                BeliTiketForm beli = new BeliTiketForm();
-                this.Hide();
-                beli.Show();
-            }
-        }
-
-        #region filter & sort
-        //Button Terapkan Filter
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if(cbUrutkan.Text == "Harga Terendah")
+            #region urutkan
+            if (cbUrutkan.Text == "Harga terendah")
             {
                 dataGridView1.Sort(dataGridView1.Columns["hargaPerTiket"], ListSortDirection.Ascending);
             }
 
-            if(cbUrutkan.Text == "Durasi Tercepat")
-            {
-                dataGridView1.Sort(dataGridView1.Columns["durasiPenerbangan"], ListSortDirection.Ascending);
-            }
-
-            if(cbUrutkan.Text == "Keberangkatan Paling Awal")
+            if (cbUrutkan.Text == "Keberangkatan paling awal") 
             {
                 dataGridView1.Sort(dataGridView1.Columns["tanggalWaktuKeberangkatan"], ListSortDirection.Ascending);
             }
 
-            if (cbUrutkan.Text == "Keberangkatan Paling Akhir")
+            if (cbUrutkan.Text == "Keberangkatan paling akhir")
             {
                 dataGridView1.Sort(dataGridView1.Columns["tanggalWaktuKeberangkatan"], ListSortDirection.Descending);
             }
 
-            fillDGV();
-
-            #region checkBox
-            if (check1.Checked == true)
+            if(cbUrutkan.Text == "Kedatangan paling awal")
             {
-                CurrencyManager cm = (CurrencyManager)BindingContext[dataGridView1.DataSource];
-                foreach (DataGridViewRow row in dataGridView1.Rows)
-                {
-                    cm.SuspendBinding();
-                    var tanggal = DateTime.Parse(Convert.ToDateTime(row.Cells["tanggalWaktuKeberangkatan"].Value).ToString("HH:mm"));
-                    if (tanggal < DateTime.Parse("00:00:00") && tanggal > DateTime.Parse("00:06:00"))
-                    {
-                        row.Visible = false;
-                    }
-                }
+                dataGridView1.Sort(dataGridView1.Columns["waktuSampai"] , ListSortDirection.Ascending);
             }
 
-            if(check2.Checked == true)
+            if (cbUrutkan.Text == "Kedatangan paling akhir")
             {
-                CurrencyManager cm = (CurrencyManager)BindingContext[dataGridView1.DataSource];
-                foreach (DataGridViewRow row in dataGridView1.Rows)
-                {
-                    cm.SuspendBinding();
-                    var tanggal = DateTime.Parse(Convert.ToDateTime(row.Cells["tanggalWaktuKeberangkatan"].Value).ToString("HH:mm"));
-                    if (tanggal < DateTime.Parse("00:06:00") && tanggal > DateTime.Parse("00:12:00"))
-                    {
-                        row.Visible = false;
-                    }
-                }
+                dataGridView1.Sort(dataGridView1.Columns["waktuSampai"], ListSortDirection.Descending);
             }
 
-            if (check3.Checked == true)
+            if (cbUrutkan.Text == "Durasi tercepat")
             {
-                CurrencyManager cm = (CurrencyManager)BindingContext[dataGridView1.DataSource];
-                foreach (DataGridViewRow row in dataGridView1.Rows)
-                {
-                    cm.SuspendBinding();
-                    var tanggal = DateTime.Parse(Convert.ToDateTime(row.Cells["tanggalWaktuKeberangkatan"].Value).ToString("HH:mm"));
-                    if (tanggal < DateTime.Parse("00:12:00") && tanggal > DateTime.Parse("00:18:00"))
-                    {
-                        row.Visible = false;
-                    }
-                }
+                dataGridView1.Sort(dataGridView1.Columns["durasiPenerbangan"], ListSortDirection.Ascending);
             }
-
-            if (check4.Checked == true)
-            {
-                CurrencyManager cm = (CurrencyManager)BindingContext[dataGridView1.DataSource];
-                foreach (DataGridViewRow row in dataGridView1.Rows)
-                {
-                    cm.SuspendBinding();
-                    var tanggal = DateTime.Parse(Convert.ToDateTime(row.Cells["tanggalWaktuKeberangkatan"].Value).ToString("HH:mm"));
-                    if (tanggal < DateTime.Parse("00:18:00") && tanggal > DateTime.Parse("00:24:00"))
-                    {
-                        row.Visible = false;
-                    }
-                }
-            }
-            fillDGV();
             #endregion
+
+            #region checkBoxes
+            if (
+                checkBox1.Checked == true ||
+                checkBox2.Checked == true ||
+                checkBox3.Checked == true ||
+                checkBox4.Checked == true)
+            {
+                CurrencyManager cm = (CurrencyManager)BindingContext[dataGridView1.DataSource];
+                cm.SuspendBinding();
+
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    row.Visible = false;
+                    var tanggal = DateTime.Parse(Convert.ToDateTime(row.Cells["tanggalWaktuKeberangkatan"].Value).ToString("HH:mm"));
+
+                    if (checkBox1.Checked == true)
+                    {
+                        if (tanggal > DateTime.Parse("00:00:00") && tanggal < DateTime.Parse("06:00:00"))
+                        {
+                            row.Visible = true;
+                        }
+                    }
+
+                    if (checkBox2.Checked == true)
+                    {
+                        if (tanggal > DateTime.Parse("06:00:00") && tanggal < DateTime.Parse("12:00:00"))
+                        {
+                            row.Visible = true;
+                        }
+                    }
+
+                    if (checkBox3.Checked == true)
+                    {
+                        if (tanggal > DateTime.Parse("12:00:00") && tanggal < DateTime.Parse("18:00:00"))
+                        {
+                            row.Visible = true;
+                        }
+                    }
+
+                    if (checkBox4.Checked == true)
+                    {
+                        if (tanggal > DateTime.Parse("18:00:00") && tanggal < DateTime.Parse("24:00:00"))
+                        {
+                            row.Visible = true;
+                        }
+                    }
+                }
+
+                cm.ResumeBinding();
+            }
+            #endregion
+
         }
         #endregion
+
+        #region utils
+
+        void fillDGV()
+        {
+            this.jadwalPenerbanganTableAdapter.Fill(this.bromoRunMarchDataSet.JadwalPenerbangan);
+            CurrencyManager cm = (CurrencyManager)BindingContext[dataGridView1.DataSource];
+            cm.SuspendBinding();
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                row.Visible = true;
+
+                var bandaraAsalID = repoListPenerbangan.bandaraAsal.id;
+                var bandaraTujuanID = repoListPenerbangan.bandaraTujuan.id;
+                var tanggal = Convert.ToDateTime( repoListPenerbangan.tanggal );
+
+                var rowBandaraAsal = row.Cells["bandaraKeberangkatanID"].Value;
+                var rowBandaraTujuan = row.Cells["bandaraTujuanID"].Value;
+                var rowTanggal = Convert.ToDateTime( row.Cells["tanggalWaktuKeberangkatan"].Value );
+
+                if (
+                    rowBandaraAsal.ToString() != bandaraAsalID.ToString() || 
+                    rowBandaraTujuan.ToString() != bandaraTujuanID.ToString() ||
+                    tanggal.ToString("dd-MM-yyyy") != rowTanggal.ToString("dd-MM-yyyy"))
+                {
+                    row.Visible = false;
+                }
+
+                var rowMaskapai = row.Cells["maskapaiID"].Value;
+                int durasi = Convert.ToInt32( row.Cells["durasiPenerbangan"].Value );
+
+                var maskapai = Repo.entities.Maskapais.Find(rowMaskapai).nama;
+                var bandaraAsal = Repo.entities.Bandaras.Find(rowBandaraAsal).nama;
+                var bandaraTujuan = Repo.entities.Bandaras.Find(rowBandaraTujuan).nama;
+
+                int jam = 0;
+                while (durasi - 60 > 0)
+                {
+                    jam++;
+                    durasi -= 60;
+                }
+                var waktuSampai = rowTanggal.AddHours(jam).AddMinutes(durasi);
+
+                row.Cells["maskapai"].Value = maskapai;
+                row.Cells["bandaraAsal"].Value = bandaraAsal;
+                row.Cells["bandaraTujuan"].Value = bandaraTujuan;
+                row.Cells["tanggalKeberangkatan"].Value = rowTanggal.ToString("dd-MM-yyyy");
+                row.Cells["waktuKeberangkatan"].Value = rowTanggal.ToString("HH:mm") + " - " + waktuSampai.ToString("HH:mm");
+                row.Cells["waktuSampai"].Value = waktuSampai;
+            }
+
+            cm.ResumeBinding();
+        }
+
+        void toBeliTiket()
+        {
+            BeliTiketForm beli = new BeliTiketForm();
+            this.Hide();
+            beli.Show();
+        }
+        #endregion
+
+        #region buttons
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            MainFormCust cust = new MainFormCust();
+            cust.Show();
+            this.Hide();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var rowIndex = e.RowIndex;
+            var row = dataGridView1.Rows[rowIndex];
+            var id = row.Cells["id"].Value;
+
+            if (dataGridView1.Columns[e.ColumnIndex].Name == "beliTiket")
+            {
+                var model = Repo.entities.JadwalPenerbangans.Find(id);
+                repoBeliTiket.jadwalPenerbangan = model;
+                toBeliTiket();
+            }
+        }
+        #endregion
+
+        private void ListPenerbanganForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
     }
 }
